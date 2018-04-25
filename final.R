cc2a7ff2106fc8a7cb0b9c03818aa014dd963e88
lwendelb <34950982+lwendelb@users.noreply.github.com>
2018-04-25 14:57:39 -0400

    Add files via upload

--- final.R
+++ final.R
@@ -25,6 +25,16 @@ for (i in 1:length(uniq)){
 }
 
 
+convert_ltv <- function(ltv){
+  ifelse(l)
+  for (i in 1:lenth(ltv)){
+    if(ltv[i] <= 80){
+      ltv[i] <- 0
+    }
+    if(80 < ltv[i] <= 85)
+  }
+}
+
 
 
 ######### Parker's code
@@ -48,8 +58,11 @@ dat$flag <- ifelse(dat$delinq_status == "X" | dat$delinq_status == "0", 0, 1)
 
 
 # for_mcmc changed
-for_mcmc <- summaryBy(flag ~ loan_number + ltv + fico + dti + acq_quarter, FUN=c(max), data=dat)
+for_mcmc <- summaryBy(flag ~ loan_number + ltv + fico + dti + acq_quarter + num_date.min + msa, FUN=c(max), data=dat)
 for_mcmc$acq_year <- as.numeric(substr(as.character(for_mcmc$acq_quarter), 1, 4))
+
+origin <- "1970-01-01"
+for_mcmc$tree <- as.Date(for_mcmc$num_date.min,origin = origin)
 
 # flag origination-year groups
 for_mcmc$early_orig <- ifelse(for_mcmc$acq_year <= 2003, 1, 0)
@@ -60,8 +73,23 @@ for_mcmc$rand <- runif(nrow(for_mcmc), 0, 1)
 for_mcmc <- for_mcmc[order(for_mcmc$rand),]
 for_mcmc <- for_mcmc[1:1000,]
 
+for_mcmc$ltv <- ifelse(for_mcmc$ltv<=80,80,for_mcmc$ltv)
+
+library(lubridate)
+library(zoo)
+for_mcmc$quarter_temp <- quarters(for_mcmc$tree)
+for_mcmc$year_temp <- year(for_mcmc$tree)
+for_mcmc$yq_temp <- paste(for_mcmc$quarter_temp, "/", for_mcmc$year_temp, sep="")
+for_mcmc$yq <- as.Date(as.yearqtr(for_mcmc$yq_temp, format = "Q%q/%Y"))
+
+
+##### housing data things
+housingdat <- read.csv("housing next five flag.csv")
+
+mcmc_data <- merge(for_mcmc, housingdat, by.x=c("msa","yq"), by.y=c("msa","yq"),all.x=T)
+
 Y = for_mcmc[,6]
-X = cbind(for_mcmc[,2:4],for_mcmc[,8:10])
+X = cbind(for_mcmc$ltv,for_mcmc$fico,for_mcmc$dti,for_mcmc$early_orig,for_mcmc$mid_orig,for_mcmc$late_orig)
 
 n = length(Y)
 
@@ -73,7 +101,7 @@ logistic_model <- "model{
 
 for(i in 1:n){
 Y[i] ~ dbern(q[i])
-logit(q[i]) <- beta[1] + beta[2]*X[i,1] + beta[3]*X[i,2] + beta[4]*X[i,3] + gamma[1]*X[i,4]
+logit(q[i]) = alpha[i] + beta[1]*X[i,1] + beta[2]*X[i,2] + beta[3]*X[i,3] + gamma[1]*X[i,4]
       + gamma[2]*X[i,5] + gamma[3]*X[i,6]
 }
 
@@ -82,23 +110,36 @@ for(j in 1:3){
   gamma[j] ~ dnorm(mug[j],taug[j])
 }
 
+
+beta[1:p] ~ dmnorm(mub[],sigma[,])
+
+
 #Priors
 
-for(j in 1:4){
-beta[j] ~ dnorm(0,0.1)
+for(i in 1:n){
+alpha[i] ~ dnorm(0,0.01)
 }
+
 
 for(j in 1:3){
 mug[j] ~ dnorm(0,0.01)
 taug[j] ~ dgamma(0.01,0.01)
 }
 
+for(j in 1:p){
+mub[j] ~ dnorm(0,0.01)
+}
+
+k = p+0.1
+sigma[1:p,1:p] ~ dwish(R[,],k)
+for(j1 in 1:p){for(j2 in 1:p){R[j1,j2]<-0.1*equals(j1,j2)}} #R is diagonal
+
 }"
 
 
 library(rjags)
 
-datlist   <- list(Y=Y,n=n,X=X)
+datlist   <- list(Y=Y,n=n,X=X,p=3)
 model <- jags.model(textConnection(logistic_model),data = datlist,n.chains=1)
 
 update(model, 10000)
